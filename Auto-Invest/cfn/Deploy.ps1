1a22afed2cf91499c75ea79d7ce29bf9bc34946d
Warrenn Enslin <warrenn.enslin@lifelinesneuro.com>
2022-05-03 18:19:43 +0200

    deployment changes

--- Auto-Invest/cfn/Deploy.ps1
+++ Auto-Invest/cfn/Deploy.ps1
@@ -9,7 +9,7 @@ param (
 
 $IpAddress = (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
 if([string]::IsNullOrEmpty($VpcId)){
-    $VpcId = $(ConvertFrom-Json([string]::Join("",$(aws ec2 describe-vpcs --filters Name=isDefault,Values=true --query "Vpcs[].VpcId" --region $Region --profile $AwsProfile))))[0]
+    $VpcId = $(ConvertFrom-Json([string]::Join("",$(aws ec2 describe-vpcs --query "Vpcs[].VpcId" --region $Region --profile $AwsProfile))))[0]
 }
 
 $SubnetId = $(aws ec2 describe-subnets --filters Name=vpc-id,Values=$VpcId --query "Subnets[0].SubnetId" --region $Region --profile $AwsProfile)
